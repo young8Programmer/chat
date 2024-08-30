53595ecb9774eedc5bd9278a04b5b584d1bfd1ad
young8programmes <valireyimbergenov79@gmail.com>
2024-08-30 16:16:48 +0500

    chat apdate

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const { Server } = require("socket.io")
+const app = express()
+const PORT = 7000
+
+app.use(express.static(__dirname + "/public"))
+
+const server = app.listen(PORT, () => {
+  console.log(`Server ${PORT} portda ishga tushdi`)
+})
+
+const io = new Server(server)
+let users = 0
+
+io.on("connection", (socket) => {
+  users++
+  console.log(`User connected: ${socket.id}`)
+  
+  socket.on("new-user", (name) => {
+    socket.name = name
+    io.emit("user-connected", name)
+    io.emit("user-count", { count: users })
+    socket.emit("welcome", name)
+  })
+
+  socket.on("disconnect", () => {
+    users--
+    io.emit('user-left', socket.name || 'User')
+    io.emit('user-count', { count: users })
+  })
+
+  socket.on("user-message", ({ name, text }) => {
+    socket.broadcast.emit("user-message-send", { name, text })
+  })
+
+  socket.on("typing-user", (name) => {
+    socket.broadcast.emit("typing-user-send", name)
+  })
+})
---
+++ src/public/app.js
@@ -0,0 +1,70 @@
+const socket = io("http://localhost:7000")
+let name = ""
+while (!name) {
+  name = prompt("Ismingizni kiriting")
+}
+
+let chatMessages = document.querySelector(".chat-messages")
+let messageForm = document.querySelector(".message-form")
+let input = document.querySelector(".input")
+let typing = document.querySelector(".typing")
+let countChat = document.querySelector(".chat-title-status-text")
+
+socket.emit("new-user", name)
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let text = input.value
+  input.value = ""
+  let p = document.createElement("p")
+  p.textContent = `Siz: ${text}`
+  p.classList.toggle("message")
+  chatMessages.append(p)
+  socket.emit("user-message", { name, text })
+})
+
+input.addEventListener("input", () => {
+  socket.emit("typing-user", name)
+})
+
+socket.on("welcome", (yourName) => {
+  if (yourName === name) {
+    let p = document.createElement("p")
+    p.textContent = `Siz guruhga qo'shildingiz`
+    p.classList.toggle("success")
+    chatMessages.append(p)
+  }
+})
+
+socket.on("user-connected", (otherName) => {
+  if (otherName !== name) {
+    let p = document.createElement("p")
+    p.textContent = `${otherName} guruhga qo'shildi`
+    p.classList.toggle("success")
+    chatMessages.append(p)
+  }
+})
+
+socket.on("user-left", (name) => {
+  let p = document.createElement("p")
+  p.textContent = `${name} guruhdan chiqdi`
+  p.classList.toggle("user-left")
+  chatMessages.append(p)
+})
+
+socket.on("typing-user-send", (name) => {
+  typing.textContent = `${name} yozmoqda...`
+  setTimeout(() => {
+    typing.textContent = ""
+  }, 2000)
+})
+
+socket.on("user-message-send", ({ name, text }) => {
+  let p = document.createElement("p")
+  p.textContent = `${name}: ${text}`
+  chatMessages.append(p)
+})
+
+socket.on("user-count", ({ count }) => {
+  countChat.textContent = `Foydalanuvchilar: ${count}`
+})
