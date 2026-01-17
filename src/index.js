3d6a54e580c9004cbb2ed1c3f3d225831b50d019
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:04 +0500

    docs: changelog yangilandi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // database querylarni optimallashtirish
 // environment variables sozlandi
 const express = require("express")
+// package.json yangilandi
 // product catalog funksiyasi qo'shildi
 // prettier formatlash
 // caching mexanizmi qo'shildi
