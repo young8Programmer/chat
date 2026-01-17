d964dccb0713822af9f460535d0dae87d987d45d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:00 +0500

    refactor: database querylarni optimallashtirish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // environment variables sozlandi
 const express = require("express")
 // prettier formatlash
+// kod uslubini yaxshilash
 // database querylarni optimallashtirish
 // package.json yangilandi
 // database connection muammosi hal qilindi
