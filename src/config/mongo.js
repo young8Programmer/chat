a2384813154dfffb24d040cc44129abe9916bfea
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:12:39 +0500

    fix: authentication xatosi tuzatildi

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose")
 const mongo = async() => {
 // kod formatlash va tozalash
 // validation xatolari tuzatildi
+// package.json yangilandi
 // CORS xatosi tuzatildi
     return await mongoose.connect("mongodb://localhost:27017/onlineChat")
 }
