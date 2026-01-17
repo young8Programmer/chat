68a47e0273aa7e4eebe5483e2a9a1e6eed68f17c
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:31 +0500

    perf: caching mexanizmi qo'shildi

--- src/public/app.js
+++ src/public/app.js
@@ -6,6 +6,7 @@ let name = ""
 while (!name) {
   name = prompt("Ismingizni kiriting")
 // kod uslubini yaxshilash
+// kod formatlash va indentatsiya
 }
 
 let chatMessages = document.querySelector(".chat-messages")
