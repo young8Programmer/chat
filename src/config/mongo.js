6a9814dbfa4be9eedad9f3cede9a44710478b7a6
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:12:50 +0500

    feat: user authentication qo'shildi

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,5 +1,6 @@
 // database connection muammosi hal qilindi
 // database migrations yaratildi
+// API endpoint testlari qo'shildi
 // routing muammosi hal qilindi
 // prettier formatlash
 // authentication xatosi tuzatildi
