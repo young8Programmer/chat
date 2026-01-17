2fe8a50661b9cfee9898709cbb02b5cae24f8b66
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:13 +0500

    refactor: kod formatlash va tozalash

--- src/models/users.js
+++ src/models/users.js
@@ -2,6 +2,7 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
+// routing muammosi hal qilindi
 // real-time notifications implementatsiya qilindi
 // component testlari yaratildi
   name: {
