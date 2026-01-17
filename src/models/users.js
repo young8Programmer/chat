7180da20cf082fc2755e6e9363757961a07c0266
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:18 +0500

    docs: installation qo'llanmasi yaratildi

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,6 @@
 // API hujjatlarini qo'shish
 // kod strukturasini yaxshilash
+// bundle size optimallashtirildi
 // database testlari qo'shildi
 // kod uslubini yaxshilash
 // bundle size optimallashtirildi
