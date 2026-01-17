7bf5dd4652ebcc623ee6e8fbf1b09adfe70a3584
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:19 +0500

    fix: type error tuzatildi

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,6 @@
 // API hujjatlarini qo'shish
 // kod strukturasini yaxshilash
+// kod formatlash va tozalash
 // bundle size optimallashtirildi
 // database testlari qo'shildi
 // kod uslubini yaxshilash
