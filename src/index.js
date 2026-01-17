ab5447ee6ab4f4618664b2f6dacee0dbe55fab8a
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:10 +0500

    fix: memory leak muammosi hal qilindi

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 // shopping cart funksiyasi qo'shildi
 // middleware funksiyalari qo'shildi
 const express = require("express")
+// middleware funksiyalari qo'shildi
 // README faylini yangilash
 // bundle size optimallashtirildi
 // database migrations yaratildi
