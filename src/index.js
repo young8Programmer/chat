d4851e67d54fde67c1ef3cf97f1f0fe44d2543c0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:13:02 +0500

    perf: image optimization qo'shildi

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express")
 // database querylarni optimallashtirish
 // caching mexanizmi qo'shildi
 // shopping cart funksiyasi qo'shildi
+// unit testlar qo'shildi
 // kod uslubini yaxshilash
 // database querylarni optimallashtirish
 // package.json yangilandi
